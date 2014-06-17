259967ff42cc97da7c4999461fa3c9a500db471e
Jon Wood <jon@ninjagiraffes.co.uk>
2014-06-17 11:17:27 +0100

    Handle phone calls

--- web.rb
+++ web.rb
@@ -3,18 +3,28 @@ require 'twilio-ruby'
 require 'json'
 
 post '/voice' do
-  puts "Got a call"
+  target = "+447429498279"
+  puts "Call received from #{params[:From]}. Forwarding to #{target}."
+
+  response = Twilio::TwiML::Response.new do |r|
+    r.Say "Your call is being redirected"
+    r.Dial do |d|
+      d.Number target
+    end
+  end
+
+  response.text
 end
 
 post '/sms' do
-  $stderr.puts params.to_json
+  target = "+447429498279"
+  puts "SMS received from #{params[:From]}. Forwarding to #{target}."
 
   response = Twilio::TwiML::Response.new do |r|
-    r.Message(to: "+447429498279", from: params[:To]) do |m|
+    r.Message(to: target, from: params[:To]) do |m|
       m.Body params[:Body]
     end
   end
-  $stderr.puts response.text
 
   response.text
 end
